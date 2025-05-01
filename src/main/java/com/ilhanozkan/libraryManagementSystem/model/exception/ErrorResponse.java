299eb6aecd20154a9b6a08aa456cd5ab2d161791
Ilhan Ozkan <contact.ilhanozkan@gmail.com>
2025-05-02 01:53:33 +0300

    Create global exception handler

---
+++ src/main/java/com/ilhanozkan/libraryManagementSystem/model/exception/ErrorResponse.java
@@ -0,0 +1,12 @@
+package com.ilhanozkan.libraryManagementSystem.model.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+  private String message;
+}
